9f52c7368f44ed0dcd00415e854380ce5adbed3b
Jennifer Browning <browningjen@yahoo.com>
2018-11-14 15:28:43 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.c17c138a5e28acba300a91c8cd5acd69.js"
+  "/%7Btic-tac-toe%7D/precache-manifest.7ede357018b821d74c9ea6b409c5a79a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/%7Btic-tac-toe%7D/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
